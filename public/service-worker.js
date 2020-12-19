91940fae2f11d20e43359433f83496bded51db09
Peyton Schlafley <pschlafley0@gmail.com>
2020-12-19 15:16:14 -0500

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,32 +22,32 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME)
+      caches.open(DATA_CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + DATA_CACHE_NAME)
         return cache.addAll(FILES_TO_CACHE);
       })
     )
 });
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function(keyList) {
-            let cacheKeepList = keyList.filter(function(key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeepList.push(CACHE_NAME);
+// self.addEventListener('activate', function (e) {
+//     e.waitUntil(
+//         caches.keys().then(function(keyList) {
+//             let cacheKeepList = keyList.filter(function(key) {
+//                 return key.indexOf(APP_PREFIX);
+//             });
+//             cacheKeepList.push(CACHE_NAME);
 
-            return Promise.all(
-                keyList.map(function(key, i) {
-                    if(cacheKeepList.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
-        })
-    )
-});
+//             return Promise.all(
+//                 keyList.map(function(key, i) {
+//                     if(cacheKeepList.indexOf(key) === -1) {
+//                         console.log('deleting cache : ' + keyList[i]);
+//                         return caches.delete(keyList[i]);
+//                     }
+//                 })
+//             );
+//         })
+//     )
+// });
 
 
 
@@ -100,7 +100,7 @@ self.addEventListener("fetch", function(event) {
         });
       })
     );
-  });
+});
 
   // Delete outdated caches
 self.addEventListener('activate', function (e) {
